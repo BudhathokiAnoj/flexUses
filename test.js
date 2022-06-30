f879491ada82597c483fc0ebb0768478a28a9575
budhathokianoj <anojbudathoki17@gmail.com>
2022-06-30 09:38:35 +0545

    first commit

---
+++ test.js
@@ -0,0 +1,22 @@
+function helloWorld(){
+    console.log("Hello world");
+}
+helloWorld();
+let a = '15';
+console.log("The number is :", a);
+
+
+const str = "Anoj Budathoki";
+console.log("My name is :",str);
+
+let array = [1,2,3,4,5,6,7,8];
+console.log("The array is:",array);
+
+let f = {name:"Hari"};
+console.log(`He is:${f.name}`);
+
+for(let a=0;a<10;a++){
+    console.log(a);
+}
+
+
